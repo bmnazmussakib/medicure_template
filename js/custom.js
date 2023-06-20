af6e7ef699785aec89c30a94759cd804c827c32d
bmnazmussakib <nazmussakib161@gmail.com>
2023-06-20 18:38:26 +0600

    home page

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $('.testimonial-slider-wrapper').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-chevron-left'></i></button>",
+    nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa-solid fa-chevron-right'></i></i></button>"
+  });
+
+});
+
